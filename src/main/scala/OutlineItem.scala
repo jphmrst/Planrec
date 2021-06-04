9670ee6e62c80df6906f4bda49f55d9f3b3686a7
John Maraist <from-github@maraist.org>
2021-06-04 13:43:37 -0500

    Pruning checkpoint

--- src/main/scala/OutlineItem.scala
+++ src/main/scala/OutlineItem.scala
@@ -10,62 +10,26 @@
 
 package org.maraist.outlines
 import java.io.PrintWriter
-import org.maraist.latex.{LaTeXdoc,LaTeXRenderable}
 
 class OutlineItem[E](val heading: E, val items: Seq[OutlineItem[E]],
                      val summary: Option[E] = None)
-extends LaTeXRenderable {
+ {
 
   def formatAsItem(
     dest: PrintWriter, lead: String, prefix: String = ""
-  )(
-    using controls: OutlineOptions
   ): Unit = {
-    if (controls.summarize) {
-      summary match {
-        case Some(s) => dest.println(lead + prefix + s)
-        case None => fullFormatAsItem(dest, lead, prefix)
-      }
-    } else {
-      fullFormatAsItem(dest, lead, prefix)
-    }
   }
 
-  def fullFormatAsItem(dest:PrintWriter, lead:String, prefix:String)(
-    using controls: OutlineOptions
-  ): Unit = {
+  def fullFormatAsItem(dest:PrintWriter, lead:String, prefix:String): Unit = {
     dest.println(lead + prefix + heading)
     val sublead = "  " + lead
     for(item <- items) {
       item.formatAsItem(dest, sublead)
     }
   }
-
-  override def toLaTeX(doc:LaTeXdoc): Unit = {
-    doc ++= heading.toString()
-    doc ++= "\n"
-    items.size match {
-      case 0 => { }
-      case n => {
-        doc ++= "\\begin{itemize}\n"
-        for(item <- items) {
-          doc ++= "\\item "
-          item.toLaTeX(doc)
-        }
-        doc ++= "\\end{itemize}\n"
-      }
-    }
-  }
 }
 
 object OutlineItem {
-  def apply[E](e: E, items: OutlineItem[E]*): OutlineItem[E]
-    = new OutlineItem[E](e, items)
-  def item[E](e: E, items: OutlineItem[E]*): OutlineItem[E]
-    = new OutlineItem[E](e, items)
-  def summarized[E](summary: E, e: E, items: OutlineItem[E]*): OutlineItem[E]
-    = new OutlineItem[E](e, items, Some(summary))
-
   given basicToItem[E]: Conversion[E, OutlineItem[E]] =
     new OutlineItem(_, Seq())
   given givenSeqToItem[E]: Conversion[Seq[E], OutlineItem[E]] with
