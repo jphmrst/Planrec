6df582ff2c039d6f79d150bf9285ef4334099126
John Maraist <from-github@maraist.org>
2021-07-20 17:05:52 -0500

    Implement check for library well-formedness.  Partially checked at this point (commit in tests forthcoming)

--- src/main/scala/yappr/Yappr.scala
+++ src/main/scala/yappr/Yappr.scala
@@ -9,7 +9,7 @@
 // language governing permissions and limitations under the License.
 
 package org.maraist.planrec.yappr
-import scala.collection.mutable.Queue
+import scala.collection.mutable.{HashSet,Queue}
 import org.maraist.planrec.
   {Recognizer, PreparedPlanLibrary, RecognitionSession, Explanation}
 import org.maraist.planrec.rules.HTNLib
@@ -37,24 +37,59 @@ class YapprErr[T, H](s: String) extends RuntimeException(s) {
 
 object YapprErrs {
   class MultiYapprErrs[T,H](val errs: Seq[YapprErr[T,H]])
-      extends YapprErr("Multiple exceptions")
-  class UnguardedLibraryRecursion[T,H](val onTerm: T)
-      extends YapprErr("Unguarded recursion inplan library.")
+      extends YapprErr[T,H]("Multiple exceptions")
+  class UnguardedLibraryRecursion[T,H](val onTermHead: H)
+      extends YapprErr[T,H]("Unguarded recursion in plan library.")
 }
 
 object Yappr extends Recognizer
   [HTNrule, HTNLib, PFFGlib, YapprSession, YapprExpl, YapprErr] {
+
   override def validLibrary[T, H](lib: HTNLib[T, H])
     (using impl: TermImpl[T, H, ?]): List[YapprErr[T, H]] = {
-    val visited: Set[T] = Set()
-    // for (top <- List()) {
-    //   val queue: Queue[T] = new Queue[T]
-    //   // queue.enqueue(???)
-    //   ???
-    // }
-
-    ???
+    import org.maraist.planrec.yappr.YapprErrs.UnguardedLibraryRecursion
+    val checked = new HashSet[H]
+    List.from(
+      for (head <- lib.ruleGoalHeads) yield checkForUnguardedLoopOnRules(
+        lib, Set(head), checked, lib.rules(head))
+    ).flatten.map(new UnguardedLibraryRecursion[T,H](_))
   }
+
+  private def checkForUnguardedLoopOnRules[T, H](
+    lib: HTNLib[T, H],
+    seen: Set[H],
+    checked: HashSet[H],
+    rules: Set[HTNrule[T, H]]
+  )(using impl: TermImpl[T, H, ?]): List[H] = {
+    val res = List.newBuilder[H]
+    for(rule <- rules) {
+      res ++= checkForUnguardedLoopOnGoals(
+        lib, seen, checked, rule.unblockedSubgoals)
+    }
+    res.result()
+  }
+
+  private def checkForUnguardedLoopOnGoals[T, H](
+    lib: HTNLib[T, H],
+    seen: Set[H],
+    checked: HashSet[H],
+    goals: Set[T]
+  )(using impl: TermImpl[T, H, ?]): List[H] = {
+    val res = List.newBuilder[H]
+    for(goal <- goals) {
+      val h = impl.head(goal)
+      if !checked.contains(h) then
+        if seen.contains(h) then
+          res += h
+        else
+          val these =
+            checkForUnguardedLoopOnRules(lib, seen + h, checked, lib.rules(h))
+          res ++= these
+        checked += h
+    }
+    res.result()
+  }
+
   override def prepareLibrary[T,H](lib: HTNLib[T, H])
     (using impl: TermImpl[T, H, ?]):
       PFFGlib[T, H] = ???
