ea897e66f8a34903b5bd28d891e40b75d82be558
John Maraist <from-github@maraist.org>
2021-09-22 15:57:28 -0500

    Progress on translating a library to an annotated NFA

--- src/main/scala/yr/HandleFinder.scala
+++ src/main/scala/yr/HandleFinder.scala
@@ -23,27 +23,44 @@ import scala.compiletime.ops.any
 
 case class Ind[T, H, S](val rule: HTNrule[T, H, S])
 
+type Annotation[T, H, S] = List[H]
+
 type Node[T, H, S] = Item[T, H, S] | H | Ind[T, H, S]
 
-type HandleFinder[T, H, S] = EdgeAnnotatedDFA[Set[Node[T, H, S]], H, Set[Unit]]
+type HandleFinder[T, H, S] =
+  EdgeAnnotatedDFA[
+    Set[Node[T, H, S]],
+    H,
+    Set[Annotation[T, H, S]]
+  ]
 
 type NondetHandleFinder[T, H, S] =
   EdgeAnnotatedNDFA[
     Node[T, H, S],
     H,
-    Unit,
-    Set[Unit],
+    Annotation[T, H, S],
+    Set[Annotation[T, H, S]],
     ? <: HandleFinder[T, H, S]
   ]
 type NondetHandleFinderBuilder[T, H, S] =
   NDFAEdgeAnnotationsBuilder[
     Node[T, H, S], H,
-    Unit, Set[Unit],
-    ?, NondetHandleFinder[T, H, S],
+    Annotation[T, H, S],
+    Set[Annotation[T, H, S]],
+    ?,
+    NondetHandleFinder[T, H, S],
     ?
   ]
 type NondetHandleFinderBuilderConcrete[T, H, S] =
-  HashEdgeAnnotatedNDFABuilder[Node[T, H, S], H, Set[Unit], Unit]
+  HashEdgeAnnotatedNDFABuilder[
+    Node[T, H, S],
+    H,
+    Set[Annotation[T, H, S]],
+    Annotation[T, H, S]
+  ]
+
+type ItemsQueue[T, H, S] =
+  Queue[(Option[(AllItem[T, H, S], H)], AllItem[T, H, S], Boolean)]
 
 object HandleFinder {
   import org.maraist.planrec.rules.HTNLib
@@ -52,72 +69,75 @@ object HandleFinder {
       NondetHandleFinder[T, H, S] = {
     val nfaBuilder = new NondetHandleFinderBuilderConcrete[T, H, S]()
 
-    // Queue for all of the items which need to be processed
-    val itemsQueue = new Queue[(Option[(Item[T, H, S], T)], Item[T, H, S])]
+    // First add all of the rule heads as stations.
+    for (head <- library.heads)
+      do if library.top.contains(head)
+         then nfaBuilder.addInitialState(head)
+         else nfaBuilder.addState(head)
 
-    // Add each rule goal term head as a state
+    // Queue for all of the items which need to be processed.
+    val itemsQueue =
+      new ItemsQueue[T, H, S]
+
+    // Process each rule, or stage it for processing.
     for (rule <- library.rules) do {
       val ruleGoalHead = rule.goal.termHead
+      val initial = initialItem(rule)
 
-      // First make sure the rule head is a state of the NFA, and is
-      // initial if appropriate.
-      if nfaBuilder.isState(ruleGoalHead)
-      then (if library.top.contains(ruleGoalHead)
-                && !nfaBuilder.isInitialState(ruleGoalHead)
-            then nfaBuilder.addInitialState(ruleGoalHead))
-      else if library.top.contains(ruleGoalHead)
-      then nfaBuilder.addInitialState(ruleGoalHead)
-      else nfaBuilder.addState(ruleGoalHead)
+      // Add the rule station and initial item to the NFA, with an
+      // epsilon transition from the station to that item.
+      nfaBuilder.addState(initial)
+      nfaBuilder.addETransition(ruleGoalHead, initial)
 
-      // Add the rule station and initial item to the NFA, and queue
-      // the initial item for further procesing.  We maintain the
-      // invariant that all items in the processing queue are already
-      // nodes in the NFA.
-      itemsQueue.enqueue((None, initialItem(rule)))
+      // For One- and Act-rules, we can just add the related items
+      // right away.  Otherwise for All-rules, we queue the initial
+      // item.  Note that we maintain the invariant that all items in
+      // the processing queue are already nodes in the NFA.
+      rule match {
+        case r: One[T, H, S] => {
+          val finalItem = OneItem(r, true)
+          nfaBuilder.addState(finalItem)
+          // OK to use triggers instead of actionHints here because
+          // they all lead to the finalItem.
+          for (t <- initial.triggers)
+            do nfaBuilder.addTransition(initial, t, finalItem)
+        }
+        case r: Act[T, H, S] => {
+          val finalItem = ActItem(r, true)
+          nfaBuilder.addState(finalItem)
+          nfaBuilder.addTransition(initial, r.action.termHead, finalItem)
+        }
+        case r: All[T, H, S] => {
+          initial match {
+            case i: AllItem[T, H, S] => {
+              val isMulti = (i.actionHints.size > 1)
+
+              // TODO If multi, then add annotations to epsilon
+              // transition for the subgoals.
+              ???
+
+              itemsQueue.enqueue((None, i, isMulti))
+            }
+
+            case _ => { } // Other cases not possible, even though
+                          // Scala cannot detect.
+          }
+        }
+      }
     }
 
     // Process the items in the queue
     while (!(itemsQueue.isEmpty))
       itemsQueue.dequeue match
-        case (prev, item) =>
-          encodeItemTransition(prev, item, nfaBuilder, itemsQueue)
+        case (prev, item, multi) =>
+          encodeItemTransition(prev, item, nfaBuilder, itemsQueue, multi)
 
     nfaBuilder.toNDFA
   }
 
-  /** Add the necessary components to an [[NDFABuilder]] for the `next`
-    * item (which may already have been added to the NFA), and for the
-    * transition from the `prev` item to the `next` item.
-    *
-    * @param item The item being added to the NFA.
-    * @param queue Queue of items to subsequently be added to the NFA.
-    * Additional items pairs may be pushed to the `queue`, but no
-    * pairs should be read from it.
-    */
-  def encodeItemNode[T, H, S](
-    item: Item[T, H, S],
-    nfa: NondetHandleFinderBuilder[T, H, S],
-    queue: Queue[(Option[(Item[T, H, S], T)], Item[T, H, S])]):
-      Unit = item match {
-    case allItem @ (AllItem(rule, ready)) => {
-      // Add the item as a node if it is not already in the NDA.
-      if !(nfa.isState(allItem)) then nfa.addState(allItem)
-
-      ???
-    }
-
-    case OneItem(rule, isFinal) => {
-      ???
-    }
-
-    case ActItem(rule, isFinal) => {
-      ???
-    }
-  }
-
-  /** Add the necessary components to an [[NDFABuilder]] for the `next`
-    * item (which may already have been added to the NFA), and for the
-    * transition from the `prev` item to the `next` item.
+  /** Add the necessary components to an [[NDFABuilder]] for the
+    * transition from a `prev` item to the `next` item, and queue up
+    * any succeeding transitions.
     *
     * @param prev Item from which the `next` item was derived, if any.
     * @param next The item being added to the NFA.
@@ -126,39 +146,40 @@ object HandleFinder {
     * pairs should be read from it.
     */
   def encodeItemTransition[T, H, S](
-    prev: Option[(Item[T, H, S], T)],
-    next: Item[T, H, S],
+    prev: Option[(AllItem[T, H, S], H)],
+    nextItem: AllItem[T, H, S],
     nfa: NondetHandleFinderBuilder[T, H, S],
-    queue: Queue[(Option[(Item[T, H, S], T)], Item[T, H, S])]):
-      Unit = next match {
-    case allItem @ (AllItem(rule, ready)) => {
+    queue: ItemsQueue[T, H, S],
+    succMulti: Boolean):
+      Unit = {
 
-      val prior = prev match {
-        case None => ()
-        case Some(item, trigger) => ()
-      }
-      // If there is only one ready element, then we can just add a
-      // simple item.  ***BUT*** we need a trigger for the transition
-      // itself.
+    // If there is a previous element, add a transition to it.
+    prev.map(_ match {
+      case (prevItem, trans) => nfa.addTransition(prevItem, trans, nextItem)
+    })
 
-      // Otherwise if there was only one ready element in the `prev`,
-      // or if there is no `prev`, then we need to add an indirection
-      // node since we now must synchronize two possibly-interleaved
-      // subgoals.
+    val prevMulti = prev match {
+      case None => succMulti
+      case Some(item, _) => (item.actionHints.size > 1)
+    }
 
-      // Moreover if we have more than one element of the ready set,
-      // and it is also not a subset of the predecessor, then we need
-      // to annotate with concurrent stack top spawning.
-
+    // If the resulting state has more than one ready element:
+    {
+      // If the prior state did not have multiple ready elements, then
+      // we must annotate the jump to multiple elements.
       ???
     }
 
-    case OneItem(rule, isFinal) => {
+    // Otherwise if the resulting state has zero or one ready
+    // elements:
+    {
+      // Add an epsilon transition to the station of the trigger
+      // above.
       ???
     }
 
-    case ActItem(rule, isFinal) => {
-      ???
-    }
+    // Now look at each of the action hints for the succeeding
+    // element, and enqueue elements for them.
+    ???
   }
 }
