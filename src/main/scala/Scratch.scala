9235d3a6506bfff893d660bd19124bddd18b7980
John Maraist <from-github@maraist.org>
2021-10-22 11:42:32 -0500

    Quicker debugging if this is not an App (old, App is the old way)

--- src/main/scala/Scratch.scala
+++ src/main/scala/Scratch.scala
@@ -3,7 +3,7 @@ import scala.collection.mutable.{Builder, HashMap, HashSet}
 trait MMM[X, Y]
 trait PPP[X, Y]
 trait QQQ[X, Y, Z]
-object Scratch extends App {
+object Scratch /* extends App */ {
 
   trait AA[Setter[_], Mapper[_,_], Elements[_,_], Automaton[_,_]] {
     def build[S,T](): Builder[Elements[S,T], Automaton[S,T]]
